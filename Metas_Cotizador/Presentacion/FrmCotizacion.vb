be29019866d72cbc4ff42b774c6abdb979ca2cc7
Manuel Morales <46793411+ManuelDJMS@users.noreply.github.com>
2019-10-01 15:47:19 -0500

    Update FrmCotizacion.vb

--- Metas_Cotizador/Presentacion/FrmCotizacion.vb
+++ Metas_Cotizador/Presentacion/FrmCotizacion.vb
@@ -548,7 +548,7 @@ Public Class FrmCotizacion
             Dim Reportes As New ReportDataSource("DataSet1", Data.Tables(0))
             FrmReportes.ReportViewer1.LocalReport.DataSources.Clear()
             FrmReportes.ReportViewer1.LocalReport.DataSources.Add(Datasource)
-            FrmReportes.ReportViewer1.LocalReport.ReportPath = "C:\Users\Software TI\Documents\GitHub\Metas_Cotizador\Metas_Cotizador\Reportes\CotizacionModelo.rdlc"
+            FrmReportes.ReportViewer1.LocalReport.ReportPath = "D:\Users\Software TI\Documents\GitHub\Metas_Cotizador\Metas_Cotizador\Reportes\CotizacionModelo.rdlc"
             FrmReportes.ReportViewer1.LocalReport.SetParameters(New ReportParameter() {p1, p2, p3, p4, p5, p6, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17,
                                                                      p19, p20, p21, p22, p23, p24, p25})
             FrmReportes.ReportViewer1.RefreshReport()
